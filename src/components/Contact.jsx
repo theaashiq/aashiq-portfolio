26a8c30340b157b99b8a61077d4bd6a6d7e5bb21
theaashiq <mdaashiqin2000@gmail.com>
2024-08-28 16:24:59 +0530

    updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,22 +15,25 @@ const Contact = (props) => {
         viewport={{ once: true, amount: 0.5 }}
         className='contact-container'>
         <div>
-          Contact<span></span>
+          <p className='contact-heading'>Contact<span></span></p>
           <div className='contact-Block'>
             <form>
               <div className='contact-block1'>
-                <div style={{backgroundColor: darkMode ? '#3e4042' : '#f2f2f2'}}>
-                  <input type='text' />
+                <div style={{backgroundColor: darkMode ? '#3e4042' : '#f2f2f2',
+                             borderColor: darkMode ? '#57595c' : '#bfbfbf'
+                }}>
+                  <input type='text' style={{color: darkMode ? '#c8c9c9' : '#333'}}/>
                 </div>
-                <div style={{backgroundColor: darkMode ? '#3e4042' : '#f2f2f2'}}>
-                  <input type='text' />
+                <div style={{ backgroundColor: darkMode ? '#3e4042' : '#f2f2f2', 
+                              borderColor: darkMode ? '#57595c' : '#bfbfbf'}}>
+                  <input type='text' style={{color: darkMode ? '#c8c9c9' : '#333'}}/>
                 </div>
               </div>
-              <div className='contact-blockEmail' style={{backgroundColor: darkMode ? '#3e4042' : '#f2f2f2'}}>
-                <input type='text' />  
+              <div className='contact-blockEmail' style={{backgroundColor: darkMode ? '#3e4042' : '#f2f2f2', borderColor: darkMode ? '#57595c' : '#bfbfbf'}}>
+                <input type='text' style={{color: darkMode ? '#c8c9c9' : '#333'}}/>  
               </div>
-              <div className='contact-blockMsg' style={{backgroundColor: darkMode ? '#3e4042' : '#f2f2f2'}}>
-                <textarea />  
+              <div className='contact-blockMsg' style={{backgroundColor: darkMode ? '#3e4042' : '#f2f2f2', borderColor: darkMode ? '#57595c' : '#bfbfbf'}}>
+                <textarea style={{color: darkMode ? '#c8c9c9' : '#333'}}/>  
               </div>  
               <div className='contact-blockSubmit'>
                 <input type='submit'/>
